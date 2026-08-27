10aac5d3858d6ee10ec61692acae40eb96371b7f
AngelinaSatchi <angelinasashixinga@gmail.com>
2026-08-27 14:01:57 +0100

    continua

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/side-menu-options.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
 
 
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  sublabel: string;
+  
+}
+
 @Component({
   selector: 'gifs-side-menu-options',
   imports: [],
   templateUrl: './side-menu-options.component.html',
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+  menuOptions:MenuOption[] = [
+    {
+    icon: 'fa-solid fa-chart-line',
+    label: 'Trending',
+    route: '/dashboard/treding',
+    sublabel: 'Gifs Popolares',
+    },
+    {
+    icon: 'fa-solid fa-magifying-glass',
+    label: 'Buscador',
+    route: '/dashboard/search',
+    sublabel: 'Buscar gifs',
+    },
+    
+  ]
+ }
