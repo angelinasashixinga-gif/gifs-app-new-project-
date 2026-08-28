f0b7bbac5ec81d64c699b9f3dbaf58eedac7c12a
AngelinaSatchi <angelinasashixinga@gmail.com>
2026-08-28 12:02:10 +0100

    [S6/L08 estou agora apagando as fotos]

--- src/app/gifs/pages/trending-page.component/trending-page.component.ts
+++ src/app/gifs/pages/trending-page.component/trending-page.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifListComponent } from "../../components/gifs-side-menu/gif-list/gif-list.component";
 
 
 @Component({
   selector: 'app-trending-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './trending-page.component.html',
 })
 export default class trendingPageComponent { }
