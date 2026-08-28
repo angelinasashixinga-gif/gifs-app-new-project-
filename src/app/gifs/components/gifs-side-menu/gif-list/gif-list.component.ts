b17768715e0f9c0704534ece9df78a5921088155
AngelinaSatchi <angelinasashixinga@gmail.com>
2026-08-28 12:11:06 +0100

    [S6/L08 TERMINEI ]

--- src/app/gifs/components/gifs-side-menu/gif-list/gif-list.component.ts
+++ src/app/gifs/components/gifs-side-menu/gif-list/gif-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { GifListItemComponent } from './gif-list-item/gif-list-item.component';
 
 
 
 @Component({
   selector: 'gif-list',
-  imports: [],
+  imports: [GifListItemComponent],
   templateUrl: './gif-list.component.html',
 })
 export class GifListComponent { }
