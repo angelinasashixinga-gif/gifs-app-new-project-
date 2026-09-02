41308306ac29c1ff86b9ba5f9f642e7ba6caf318
AngelinaSatchi <angelinasashixinga@gmail.com>
2026-09-02 22:40:59 +0100

    [S7/L04 DONE]

--- src/app/gifs/pages/trending-page.component/trending-page.component.ts
+++ src/app/gifs/pages/trending-page.component/trending-page.component.ts
@@ -3,20 +3,20 @@ import { GifListComponent } from "../../components/gifs-side-menu/gif-list/gif-l
 import { GifService } from '../../services/gifs.service';
 
 
-const imageUrls: string[] = [
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
-];
+// const imageUrls: string[] = [
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
+//     "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+// ];
 
 @Component({
   selector: 'app-trending-page',
@@ -24,7 +24,7 @@ const imageUrls: string[] = [
   templateUrl: './trending-page.component.html',
 })
 export default class trendingPageComponent { 
-  gifs = signal(imageUrls);
 
   gifService = inject( GifService ); 
+  
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,6 +11,7 @@ export class GifService {
 private http = inject(HttpClient);
 
 trendingGifs = signal<Gif[]>([]);
+trendingGifsLoading = signal(true);
     
 constructor() {
     this.loadTrendingGifs();
@@ -24,6 +25,8 @@ loadTrendingGifs() {
         },
     }).subscribe( (resp) => {
         const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+        this.trendingGifs.set(gifs);
+        this.trendingGifsLoading.set(false);
         console.log({ gifs });
 
         });
