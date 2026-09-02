2166fdaec3157248dad6ba8475a243d9ba5ced26
AngelinaSatchi <angelinasashixinga@gmail.com>
2026-09-02 20:37:26 +0100

    [S7/L05 erros]

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
 
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch())
   ],
 };
